6c1308cc3470a56b163367324c0e4558d83531bd
Robert Rothenberg <rrwo@cpan.org>
2024-12-26 23:28:03 +0000

    Add ceaveat on why they are listed

--- lib/Task/CPANSec.pm
+++ lib/Task/CPANSec.pm
@@ -22,6 +22,9 @@ This is a draft.
 L<Task::CPANSec> is a list of recommended modules from the CPAN Security Group (CPANSec), in the spirit of
 L<Task::Kensho>.
 
+This list is not exhaustive, but it contains modules that we believe are good solutions for their tasks, and are being
+actively maintained.
+
 =head1 CATEGORIES
 
 =head2 L<Task::CPANSec::Random>: for generating random data
